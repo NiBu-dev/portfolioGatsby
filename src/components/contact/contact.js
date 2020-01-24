828fdf0df31b73708d15cf9cf996b4aab3565c36
Nick <sahas023@gmail.com>
2020-01-24 19:55:59 +0100

    linked form to 3rd party server

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -6,12 +6,14 @@ const ContactComponent = () => {
             <div className="contact-container">
                 <h2 className="section-title">Get in touch</h2>
                 <h3 className="contact-question">Have a question or want to work together?</h3>
-                <form className="form">
-                    <input className="form-input" type="text" placeholder="Name"/>
-                    <input className="form-input" type="email" placeholder="Email"/>
-                    <textarea className="form-textarea" placeholder="Your message"/>
-                </form>
-                <button className="form-btn" type="submit">SUBMIT</button>
+                <div className="form-wrapper">
+                    <form className="form" action="https://getform.io/f/662dc2d3-7e9e-4d87-b89e-0206b0a7d513" method="POST">
+                        <input className="form-input" type="text" placeholder="Name" name="name" required/>
+                        <input className="form-input" type="email" placeholder="Email" name="email" required/>
+                        <textarea className="form-textarea" placeholder="Your message" name="message" required/>
+                        <button className="form-btn" type="submit">SUBMIT</button>
+                    </form>
+                </div>
             </div>
         </section>
     )
