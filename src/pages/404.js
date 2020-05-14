ed2cd3b72294af1afd2e987d9637b5cf7924ce27
Nick <sahas023@gmail.com>
2020-05-14 11:36:44 +0200

    modifed error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="error-container">
+      <h1 className="error-header">NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness is immeasurable.</p>
+    </div>
   </Layout>
 )
 
